2c4f8f227d9437d38f52de266df4662e2f25683c
Jonas Engberg <jonas.engberg@outlook.com>
2023-10-04 13:13:29 +0300

    testing with absolute path to assets and moved 404 to public folder

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useTranslation } from 'react-i18next';
 import LanguageToggler from './components/LanguageHandler/LanguageHandler';
 import { InfoView } from './views';
 import { Route, Routes } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const App = () => {
   const { t } = useTranslation();
@@ -12,7 +13,7 @@ const App = () => {
         <h1>{t('app.title')}</h1>
       </header>
       <LanguageToggler />
-
+      <Link to="info">Link to info page</Link>
       <Routes>
         <Route path="/info" element={<InfoView />} />
       </Routes>
