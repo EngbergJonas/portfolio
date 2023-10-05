059fa5597b71f6c9cf9b131aea0eee997a66b2bb
Jonas Engberg <jonas.engberg@outlook.com>
2023-10-05 23:38:47 +0300

    trying out tailwind responsive classes

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -6,14 +6,19 @@ import './styles.css';
 const Hero = () => {
   const { t } = useTranslation();
   return (
-    <header data-testid="hero" className="max-h-screen h-screen min-h-screen mx-20">
-      <div className="flex h-full pt-20 justify-between">
+    <header data-testid="hero" className="max-h-screen h-screen min-h-screen mx-10 md:mx-20">
+      <div className="md:flex-row flex-col flex h-full pt-8 md:pt-20 justify-between">
         {/* title, subtitle, text-content and signature */}
         <div data-testid="hero-text">
-          <h1 className="text-7xl font-medium w-full mb-2">{t('app.title')}</h1>
-          <h2 className="text-2xl font-medium ">{t('app.subtitle')}</h2>
-          <p className="text-xl my-[4rem] max-w-[54rem]">{t('app.textContent')}</p>
-          <img data-testid="signature-image" className="h-[8rem]" src={signature} alt="signature" />
+          <h1 className="text-4xl md:text-7xl font-medium w-full mb-2">{t('app.title')}</h1>
+          <h2 className="text-xl md:text-2xl font-medium">{t('app.subtitle')}</h2>
+          <p className="md:text-xl my-4 md:my-[4rem] max-w-[54rem]">{t('app.textContent')}</p>
+          <img
+            data-testid="signature-image"
+            className="h-[8rem] hidden md:block"
+            src={signature}
+            alt="signature"
+          />
         </div>
         {/* hero image */}
         <img
