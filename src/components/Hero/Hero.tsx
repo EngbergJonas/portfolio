fbf6e36b23e7415223d5fe55845777b39f3dc0ef
Jonas Engberg <jonas.engberg@outlook.com>
2023-10-05 22:43:14 +0300

    added comments to hero page

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -7,12 +7,14 @@ const Hero = () => {
   return (
     <header data-testid="hero" className="max-h-screen h-screen min-h-screen mx-20">
       <div className="flex h-full pt-20 justify-between">
+        {/* title, subtitle, text-content and signature */}
         <div data-testid="hero-text">
           <h1 className="text-7xl font-medium w-full mb-2">{t('app.title')}</h1>
           <h2 className="text-2xl font-medium ">{t('app.subtitle')}</h2>
           <p className="text-xl my-[4rem] max-w-[54rem]">{t('app.textContent')}</p>
           <img data-testid="signature-image" src={signature} alt="signature" />
         </div>
+        {/* hero image */}
         <img data-testid="hero-image" className="scale-x-[-1] w-auto" src={jonas} alt="jonas" />
       </div>
     </header>
